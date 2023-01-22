c78b1c087a1e794d1f51f031f068ebd70c0aefc3
AlexAmanitas <104567917+AlexAmanitas@users.noreply.github.com>
2023-01-22 12:34:01 +0200

    hgc

---
+++ cheapTrip/src/app/search-result/path-details/path-details.component.ts
@@ -0,0 +1,168 @@
+
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPath } from '../../service/http.service';
+import { HttpService } from 'src/app/service/http.service';
+import { GlobalService } from 'src/app/global/global.service';
+import { Currency } from 'src/app/currency-selector/currency-selector.component';
+import { IRout } from 'src/app/trip-direction/trip-direction.model';
+
+const TRANSPORT = new Map();
+TRANSPORT.set('Bus', $localize`Bus`);
+TRANSPORT.set('Flight', $localize`Flight`);
+TRANSPORT.set('Train',$localize`Train`);
+TRANSPORT.set('Ride Share',$localize`Ride Share`);
+TRANSPORT.set('Car Drive',$localize`Car Drive`);
+TRANSPORT.set('Walk',$localize`Walk`);
+TRANSPORT.set('Town Car', $localize`Town Car`);
+TRANSPORT.set('Car Ferry',$localize`:@@CarFerry:Car Ferry`);  // two variants for ferry
+TRANSPORT.set('Ferry', $localize`:@@CarFerry:Car Ferry`);      //
+TRANSPORT.set('Shuttle', $localize`Shuttle`);
+TRANSPORT.set('Taxi', $localize`Taxi`);
+
+@Component({
+  selector: 'app-path-details',
+  templateUrl: './path-details.component.html',
+  styleUrls: ['./path-details.component.scss'],
+})
+
+
+
+export class PathDetailsComponent implements OnInit {
+@Input() paths: IPath[];
+@Input() startPoint: string;
+@Input() endPoint: string;
+private country:string;
+//private currency:Currency;
+
+
+
+price: number;
+  constructor(private httpService: HttpService,
+              private globalService: GlobalService
+    ) {
+
+
+    this.httpService.getUserCountry().subscribe({
+      next: (data:any)=>{console.log("Got user country:"+data.country_code);
+        this.country= data.country_code},
+      error: (err:any)=>{console.log(err);
+        this.country= "undefined"}
+   })
+
+
+  }
+
+
+
+
+  ngOnInit(): void {
+    // this.currency = this.globalService.getCurrency();
+  }
+
+  getGlobalCurrency(){
+    return this.globalService.getCurrency();
+  }
+
+  getTransportName(transport:string) {
+    return TRANSPORT.get(transport);
+  }
+
+
+  getTransportUrl (rout:IRout){
+    let url:string='';
+    
+    switch(rout.transportation_type) { 
+      case "Flight": { 
+        //url = "http://Skyscanner.com";
+        url= this.getSkyScannerUrl(rout);
+         break; 
+      } 
+      case "Bus": { 
+        //url = "http://bus.tickets.ua";
+        url=this.getBusUrl(rout);
+         break; 
+      } 
+      case "Train": { 
+        //url = "http://gd.tickets.ua";
+         url=this.getTrainUrl(rout);
+        //url = "https://www.tutu.ru/poezda";
+        break; 
+      } 
+      case "Car Ferry": { 
+        url = "http://www.aferry.com";
+         break; 
+      } 
+      case "Ferry": { 
+        url = "http://www.aferry.com";
+         break; 
+      } 
+       case "Ride Share": { 
+        url = "http://BlaBlaCar.com";
+         break; 
+      } 
+      default: { 
+         url='';
+         break; 
+      } 
+   }
+   return url;
+  }
+  getBusUrl(rout :IRout){
+    // console.log ("bus "+this.country);
+    // if((from == 545 OR to == 545) AND (transportation_type == 2))
+    // console.log("bus new "+JSON.stringify(rout));
+    if (rout.from=="Донецк"||rout.from=="Donetsk"||
+    rout.to=="Донецк"||rout.to=="Donetsk"  ){
+      //right way is to use ID of city, but current server is not build for that
+      return "http://bustravel.dn.ua/";
+    }
+    switch(this.country){
+      case "RU":
+      case "BY":
+      
+        return "http://bus.tutu.ru";
+      case "IN":
+        return "https://www.makemytrip.com/bus-tickets/";
+      case "UA":
+      default:
+        return "https://omio.sjv.io/XxEWmb";
+    }
+  }
+
+  getTrainUrl(rout:IRout){
+    console.log ("train "+this.country);
+    switch(this.country){
+      
+      case "RU":
+      case "BY":
+      
+        return "https://www.tutu.ru/poezda";
+      case "IN":
+        return "https://www.makemytrip.com/railways/";
+      case "UA":
+      default:
+        return "https://omio.sjv.io/XxEWmb";
+    }
+  }
+
+  openTransport (rout: IRout){
+    //function for opening site of corresponding transportation type
+     let url = this.getTransportUrl(rout);
+    if (url!=''){
+      window.open(url, "_blank");
+    }
+  }
+  getSkyScannerUrl (rout:IRout){
+let from=this.httpService.getSkyScannerCode(rout.from);
+
+let to = this.httpService.getSkyScannerCode(rout.to);
+
+      if (from==""||to==""){
+      return "http://Skyscanner.com";
+      }
+      return "http://Skyscanner.com/transport/flights/"+from+"/"+to;
+
+  }
+}
+
---
+++ cheapTrip/src/environments/environment.ts
@@ -0,0 +1,32 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  mainServer:"tomcat",   // "tomcat "or "spring"
+  url68: 'http://3.18.167.68:8080/',
+  url104:  'http://3.18.167.68:8080/',
+  // urlTomCat: 'https://test2.lowcoststrip.com:8443/'
+  // urlTomCat: 'http://3.64.201.17:8080/',
+  // urlTomCat: 'https://test3.lowcoststrip.com:8443/',
+  urlTomCat: 'https://cheaptrip.guru:8443/',
+  urlGoogleFormEng: 'https://docs.google.com/forms/d/11mnL9hPZHkbixglQNqlN-iChn4-dYidnNnQAPXakYvA/viewform',
+  urlGoogleFormRus: 'https://docs.google.com/forms/d/1ypZL2BFqHmlgPrzsbg3BbOK1Ns_FBYuMrsvYoUOskJM/viewForm',
+
+  // urlTomCat: 'http://ec2-3-65-40-85.eu-central-1.compute.amazonaws.com:8080/'
+  // urlTomCat: 'http://ec2-3-134-113-37.us-east-2.compute.amazonaws.com:8080/'
+  //  url104:  'http://18.158.89.230:8080/'  //working server
+  // url104: 'http://localhost:8080/', 
+ // url104: 'http://35.238.107.151:8080/',
+  // url104: 'http://ec2-3-134-113-37.us-east-2.compute.amazonaws.com:8080/', 
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
